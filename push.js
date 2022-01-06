4f51acfc9c4921d8f11009b0dcf2ae39b0a7e084
unknown <nabil.nnn@outlook.sa>
2022-01-06 21:32:23 +0200

    fix

--- push.js
+++ push.js
@@ -6,5 +6,14 @@ let vapidkeys = {
   privateKey: "gW2qwbSo7JFxFp0h2O_TBSA7RxJwKBBrslPCCcAeZw4",
 };
 push.setVapidDetails('mailto:test@code.co.uk', vapidkeys.publicKey, vapidkeys.privateKey);
-let sub = {};
+let sub = {
+  endpoint:
+    "https://fcm.googleapis.com/fcm/send/cg0B4yUQmsg:APA91bG50jwWy2Cq-o80-3J209cKN_KAg26fUUztYsoNwbzcbTWyXRqLfloO8QmNihmfBv_EKo0PP6UQiBU88RGA7w8TrI9gMQjZszcknw0ViCqEG1Klzi2uUrryJz1Dz2fDb4cMUjq3",
+  expirationTime: null,
+  keys: {
+    p256dh:
+      "BD8j5j9QQMN_-10fd6YrXCTExmttLjeyF_8pR7RxD-hHibj71HYHbvpGZdTxV60eIUeY9wOyarifbihGYbHz3JM",
+    auth: "8W1FxNcha0LHpO5ihiKFLA",
+  },
+};
 push.sendNotification(sub , "test message")
--- sw.js
+++ sw.js
@@ -1,3 +1,20 @@
 self.addEventListener('push', (event) => {
-     event.registration.showNotification('Hello world!')
+     var options = {
+           body: 'This notification was generated from a push!',
+               icon: 'images/example.png',
+               vibrate: [100, 50, 100],
+               data: {
+                       dateOfArrival: Date.now(),
+                           primaryKey: 1
+                       },
+               actions: [
+                           {action: 'explore', title: 'Explore this new world',
+                               icon: 'images/checkmark.png'},
+                               {action: 'close', title: 'Close',
+                                   icon: 'images/xmark.png'},
+                           ]
+               };
+               e.waitUntil(
+                       self.registration.showNotification('Hello world!', options)
+                       );
 });
